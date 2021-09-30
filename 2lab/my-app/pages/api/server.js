3c998adf238104fce0d966e2860fec346793d11d
IlIAntonIlI <anton.adamchuk.kpi@gmail.com>
2021-09-30 14:37:21 +0300

    Added rate-limiting

--- 2lab/my-app/pages/api/server.js
+++ 2lab/my-app/pages/api/server.js
@@ -1,8 +1,19 @@
 import sanitizeHtml from "sanitize-html";
+
 const nodemailer = require("nodemailer");
 
+const rateLimit = require("lambda-rate-limiter")({
+  interval: 60 * 1000, // Our rate-limit interval, one minute
+}).check;
+
 export default async function handler(req, res) {
-  console.log(req.body);
+  try {
+    // 10 stands for the maximum amount of requests allowed during the defined interval
+    // rateLimit now returns a promise, let's await for it! (◕‿◕✿)
+    await rateLimit(3, req.headers["client-ip"]);
+  } catch (error) {
+    res.status(429).json(error); // Still returning a basic 429, but we could do anything~
+  }
   const transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
@@ -12,12 +23,12 @@ export default async function handler(req, res) {
     },
   });
 
-  let info = await transporter.sendMail({
+  await transporter.sendMail({
     from: '"Alvah 👻" <alvah.zulauf@ethereal.email>', // sender address
     to: req.body.where, // list of receivers
     subject: "Hello ✔", // Subject line
     text: sanitizeHtml(req.body.letter), // plain text body
     html: sanitizeHtml(req.body.letter), // html body
   });
-  res.status(200).json(info);
+  res.status(200).json({});
 }
