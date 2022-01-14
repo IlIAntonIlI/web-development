11378a07f7b797d17575a5db301a013a93d3a8f1
IlIAntonIlI <72148852+IlIAntonIlI@users.noreply.github.com>
2022-01-14 04:26:43 +0200

    Added 3lab
    * 3lab first variation
    
    * Rate limit 2
    
    * Fixed loading post page and some scss transformation at stylesheets files
    
    * Check loader
    
    * Fix loader
    
    * Added subscription
    
    * test
    
    * test2
    
    * test3
    
    * test4
    
    * Deleted unnecessary files and fix color variables
    
    * Fix [id] date of creation
    
    * Redeploying vercel
    
    * Fixed form's adaptivity
    
    * Added map for e.target.elements, secrects, fixed z-index, fixed alert
    
    * Fixed secrets variation1
    
    * Deleting env
    
    * Fix container for different extension
    
    * Fixed loader at [id] page, added secrets at all pages
    
    * Fixed vx alert,form; fixed ternar operators; Deleted useless state variables; Added some handling of errors
    
    * Fixed pre-commit
    
    * Simplify styles
    
    * Deleted useless state variable
    
    * Fixed alert classname.
    
    * == replaced by === . Changed adaptivity styles for form
    
    * Fixed handling errors. Some style fixes
    
    * Added finally block for set visibility. Added min clamp for vx loader
    
    * Some fixes project
    
    * Added variables into mutation.
    
    * Delete env from action

---
+++ 3lab/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    DATABASE_LINK: process.env.DATABASE_LINK,
+    SECURE_DATABASE_LINK: process.env.SECURE_DATABASE_LINK,
+  },
+};
---
+++ 3lab/pages/index.js
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import styles from "/styles/Home.module.scss";
+import Header from "/components/header/header";
+import Post from "/components/post/post";
+import Footer from "/components/footer/footer";
+import Alert from "/components/alert/alert";
+import Loader from "/components/loader/loader";
+import { useState } from "react";
+import React from "react";
+import { useSubscription } from "urql";
+
+const newPosts = `
+subscription {
+  posts(order_by: {date: desc}) {
+    author
+    id
+    postText
+    date
+    Theme
+  }
+}`;
+export default function Home() {
+  const [textOfAlert, setTextAlert] = useState("");
+  const [colorOfAlert, setColorAlert] = useState("");
+
+  const closingFunction = function () {
+    if (textOfAlert) {
+      setTextAlert("");
+    }
+  };
+  const [result] = useSubscription({
+    query: newPosts,
+  });
+
+  const { data, fetching, error } = result;
+  if (error) {
+    setTextAlert("Error while fetching data");
+    setColorAlert("red");
+  }
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Stories</title>
+      </Head>
+      <Header />
+      <Alert text={textOfAlert} color={colorOfAlert} close={closingFunction} />
+      {fetching || error ? (
+        <>
+          <main className={styles.postslist}></main>
+          <Loader />
+        </>
+      ) : (
+        <main className={styles.postslist}>
+          {data.posts.map((post) => (
+            <div key={post.id}>
+              <Post
+                theme={post.Theme}
+                id={post.id}
+                author={post.author}
+                text={post.postText}
+                date={post.date.slice(0, 10) + " | " + post.date.slice(11, 19)}
+              />
+            </div>
+          ))}
+        </main>
+      )}
+
+      <Footer />
+    </>
+  );
+}
